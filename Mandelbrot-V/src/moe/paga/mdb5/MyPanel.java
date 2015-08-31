61fb5e8cbd38df5fc3c04fb017e265b2095845a9
John Chen <johnchen902@gmail.com>
2015-08-31 12:51:08 +0800

    Changed MyPanel

--- Mandelbrot-V/src/moe/paga/mdb5/MyPanel.java
+++ Mandelbrot-V/src/moe/paga/mdb5/MyPanel.java
@@ -1,9 +1,15 @@
 package moe.paga.mdb5;
 
+import static moe.paga.mdb5.Location.Quadrant.III;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.function.BiFunction;
 
 import javax.swing.JPanel;
 import javax.swing.SwingWorker;
@@ -16,33 +22,55 @@ import moe.paga.mdb5.func.Utilities;
 @SuppressWarnings("serial")
 public class MyPanel extends JPanel {
 
-	private Image image;
+	private static final int PANEL_SIZE = 512;
+	private static final int CHUNK_SIZE = 128;
+	private Map<Location, Image> images;
 
 	public MyPanel() {
-		setPreferredSize(new Dimension(512, 512));
-		new SwingWorker<Image, Void>() {
-			@Override
-			protected Image doInBackground() throws Exception {
-				return Utilities.compose(new MandelbrotDouble().andThen(new Reverse()), new DirectImage())
-						.apply(new Location(), new Size(512, 512));
-			}
+		setPreferredSize(new Dimension(PANEL_SIZE, PANEL_SIZE));
+		images = new HashMap<>();
+	}
 
-			@Override
-			protected void done() {
-				try {
-					image = get();
-				} catch (InterruptedException | ExecutionException e) {
-					e.printStackTrace();
+	private BiFunction<Location, Size, Image> getFunction() {
+		return Utilities.compose(new MandelbrotDouble().andThen(new Reverse()), new DirectImage());
+	}
+
+	private Image getImage(Location location) {
+		if (images.containsKey(location)) {
+			return images.get(location);
+		} else {
+			new SwingWorker<Image, Void>() {
+				@Override
+				protected Image doInBackground() throws Exception {
+					return getFunction().apply(location, new Size(CHUNK_SIZE, CHUNK_SIZE));
 				}
-				repaint();
-			}
-		}.execute();
+
+				@Override
+				protected void done() {
+					try {
+						images.put(location, get());
+					} catch (InterruptedException | ExecutionException e) {
+						e.printStackTrace();
+					}
+					repaint();
+				}
+			}.execute();
+			return null;
+		}
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		if (image != null)
-			g.drawImage(image, 0, 0, this);
+
+		Location locY = new Location(Arrays.asList(III, III));
+		for (int y = 0; y < getHeight(); y += CHUNK_SIZE, locY = locY.increaseY()) {
+			Location loc = locY;
+			for (int x = 0; x < getWidth(); x += CHUNK_SIZE, loc = loc.increaseX()) {
+				Image image = getImage(loc);
+				if (image != null)
+					g.drawImage(image, x, y, this);
+			}
+		}
 	}
 }
